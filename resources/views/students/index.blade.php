7089dd5b17ff01985f86ed6a8cbcd3bf807cf153
Rebeca-JY <goyuu96@gmail.com>
2026-08-06 06:52:57 +0700

    lanjutan

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -50,8 +50,8 @@
                 </td>
                 <td class="px-5 py-4">
                     <div class="flex justify-end gap-4 text-xs font-medium">
-                        <a href="{{ route('students.show', $student['id' => 1]) }}" class="text-[#16213A] hover:text-[#A16207]">Lihat</a>
-                        <a href="{{ route('students.edit', $student['id' => 1]) }}" class="text-[#16213A] hover:text-[#A16207]">Ubah</a>
+                       <a href="{{ route('students.show', ['id' => $student['id']]) }}" class="text-[#16213A] hover:text-[#A16207]">Lihat</a>
+                       <a href="{{ route('students.edit', ['id' => $student['id']]) }}" class="text-[#16213A] hover:text-[#A16207]">Ubah</a>
                         <form action="" method="POST"
                             onsubmit="return confirm('Hapus data siswa ini dari buku induk?')">
 
